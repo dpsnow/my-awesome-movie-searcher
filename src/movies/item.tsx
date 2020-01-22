8c26464b4614716344cc502c33219924661bec2f
Daria <econs.dk@gmail.com>
2020-01-22 23:16:28 +0700

    added hooks ans styles

--- src/movies/item.tsx
+++ src/movies/item.tsx
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    Card,
+    Typography,
+    CardContent,
+    CardActions,
+    Box,
+    CardMedia,
+    CardHeader,
+    Chip,
+    Divider,
+    IconButton,
+    makeStyles,
+    Collapse,
+} from '@material-ui/core';
+
+import { Favorite as FavoriteIcon, ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
+
+function getRandomInt(max: number): number {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+// TODO: разобраться со стилями и вынести жанры в отдельный компонент
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: '2px',
+        },
+    },
+});
+
+const renderGenres = (genre: string) => {
+    return <Chip variant='outlined' key={genre} size='small' label={genre} />;
+};
 
 export const Item = (props: ItemPropsT) => {
-    const { title, overview } = props;
+    const { title, overview, genres } = props;
+
+    const [expanded, setExpanded] = useState(false);
+    const [isFavorite, setFavorite] = useState(false);
+
+    const onClickBtnExpand = () => {
+        setExpanded(!expanded);
+    };
+
+    const onClickBtnFavorite = () => {
+        setFavorite(!isFavorite);
+    };
+
+    const classes = useStyles();
+
     return (
-        <li>
-            <a href='#'>
-                <h2>{title}</h2>
-            </a>
-            <p>{overview}</p>
-        </li>
+        <Card>
+            <CardMedia style={{ height: '300px' }} image={`https://picsum.photos/id/${getRandomInt(500)}/200/300`} />
+            <CardHeader title={title} titleTypographyProps={{ align: 'center' }} />
+            <Divider variant='middle' />
+            <CardContent>
+                <Box className={classes.root}>{genres.map(renderGenres)}</Box>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton onClick={onClickBtnFavorite} color={isFavorite ? 'secondary' : 'default'}>
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton onClick={onClickBtnExpand} style={{ marginLeft: 'auto' }}>
+                    <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout='auto' unmountOnExit>
+                <CardContent>
+                    <Typography paragraph>Overview:</Typography>
+                    <Typography paragraph>{overview}</Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
     );
 };
--- src/movies/list.tsx
+++ src/movies/list.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Item } from './item';
+import { Typography, Grid } from '@material-ui/core';
 
-const renderMovies = (item: ItemPropsT) => <Item key={item.id} title={item.title} overview={item.overview} />;
+const renderMovies = (item: ItemPropsT) => {
+    return (
+        <Grid item xs={4} key={item.id}>
+            <Item title={item.title} overview={item.overview} genres={item.genres} />
+        </Grid>
+    );
+};
 
-export const List = (props: ListPropsT) => {
-    const { content } = props;
+export const List = () => {
+    const [movies, setMovies] = React.useState([]);
+
+    useEffect(() => {
+        if (movies.length === 0) {
+            fetch('https://devlab.website/v1/movies')
+                .then(res => res.json())
+                .then(movies => {
+                    // console.log('movies', movies);
+                    setMovies(movies);
+                })
+                .catch(err => {
+                    // console.log('err', err);
+                });
+        }
+    }, [movies]);
+
     return (
         <>
-            <h1>Movies</h1>
-            <ul>{content.map(renderMovies)}</ul>;
+            <Typography variant='h3' component='h1'>
+                Movies
+            </Typography>
+            <Grid container alignItems='stretch' spacing={3}>
+                {movies.map(renderMovies)}
+            </Grid>
         </>
     );
 };
