311eb7fd84614317ea79bc533a5d5b510f785582
Daria <econs.dk@gmail.com>
2020-01-26 19:27:28 +0700

    Updates

---
+++ precache-manifest.96db93bedae5b657be99529c37a817b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef470331f0838418d3d310f810211a13",
+    "url": "/my-awesome-movie-searcher/index.html"
+  },
+  {
+    "revision": "94ee1baf776719328a87",
+    "url": "/my-awesome-movie-searcher/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "563023440836886c5907",
+    "url": "/my-awesome-movie-searcher/static/js/2.c5cee01e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/my-awesome-movie-searcher/static/js/2.c5cee01e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94ee1baf776719328a87",
+    "url": "/my-awesome-movie-searcher/static/js/main.a278df3c.chunk.js"
+  },
+  {
+    "revision": "9a7b8b2fed221a158ac5",
+    "url": "/my-awesome-movie-searcher/static/js/runtime-main.8133b171.js"
+  }
+]);
