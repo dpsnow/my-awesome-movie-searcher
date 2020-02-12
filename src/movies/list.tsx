4e833acb9c511102d6783598ddf4d9793c7c6550
Daria <econs.dk@gmail.com>
2020-02-12 23:29:48 +0700

    fixed after merge

--- src/movies/list.tsx
+++ src/movies/list.tsx
@@ -3,7 +3,6 @@ import { Typography, Grid } from '@material-ui/core';
 import { fetchMovies } from '../api';
 import { useDispatch, useSelector } from 'react-redux';
 
-import * as api from '../api';
 import { getAllMovies } from '../redux-setup';
 import { selectDesiredMovies } from './selectors';
 
@@ -12,8 +11,10 @@ import { Item } from './item';
 export const List = () => {
     const dispatch = useDispatch();
 
-    const allMovies = useSelector<RootStoreT, Map<number, MovieT>>(store => store.movies);
+    // const allMovies = useSelector<RootStoreT, Map<number, MovieT>>(store => store.movies);
+    const allMovies = useSelector<RootStoreT, any>(selectDesiredMovies);
 
+    console.log(allMovies);
     useEffect(() => {
         fetchMovies().then((movies: MoviesT) => {
             dispatch(getAllMovies(movies));
@@ -27,7 +28,7 @@ export const List = () => {
             </Typography>
             <Grid container alignItems='stretch' spacing={1}>
                 {allMovies &&
-                    Array.from(allMovies.values()).map((item: MovieT) => {
+                    Array.from(allMovies.values()).map((item: any) => {
                         return (
                             <Grid item xs={12} sm={6} md={4} key={item.id}>
                                 {/* TODO: исправить септку после стилизации под макет */}
--- src/movies/selectors.ts
+++ src/movies/selectors.ts
@@ -2,15 +2,17 @@ import { createSelector } from 'reselect';
 
 const getFavMovies = (state: RootStoreT) => Array.from(state.movies.values()).filter((it: any) => it.isFav);
 
-const getDesiredMovies = (state: RootStoreT) => {
+const getDesiredMovies = (state: RootStoreT): MoviesT => {
     if (state.search === undefined) return state.movies;
 
     const { genre, text } = state.search.values;
 
     if (genre || text) {
-        return Array.from(state.movies.values())
+        const movies: any = Array.from(state.movies.values())
             .filter((movie: any) => (genre === 'all' ? true : movie.genres.includes(genre)))
-            .filter((movie: any) => (text === '' ? true : movie.title.includes(text)));
+            .filter((movie: any) => (text === '' ? true : movie.title.includes(text)))
+            .reduce((acc: any[], movie: MovieT) => acc.concat([[movie.id, movie]]), []);
+        return new Map(movies);
     }
 
     return state.movies;
